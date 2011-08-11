8ee49153572949dc9702c77025212b4c84f93932
phildo <Phildo211@gmail.com>
2011-08-11 17:32:22 +0000

    createItemAndGiveToPlayer checks for type

--- services/aris_1_5/items.php
+++ services/aris_1_5/items.php
@@ -187,7 +187,7 @@ class Items extends Module
      * @see uploadHandler.php
      */
 	public static function createItemAndGiveToPlayer($gameId, $playerId, $name, $description, 
-								$fileName, $droppable, $destroyable, $latitude, $longitude)
+								$fileName, $droppable, $destroyable, $latitude, $longitude, $fileType="NORMAL")
 	{
 		$prefix = Module::getPrefix($gameId);
 		if (!$prefix) return new returnData(1, NULL, "invalid game id");
@@ -195,32 +195,42 @@ class Items extends Module
 		$name = addslashes($name);
 		$description = addslashes($description);
 		
-		//Create the Media
-		$newMediaResultData = Media::createMedia($gameId, $name, $fileName, 0);
-		$newMediaID = $newMediaResultData->data->media_id;
-		
-		//Does game allow players to drop items?
+        //Does game allow players to drop items?
 		if ($droppable) { 
 			$game = Games::getGame($gameId);
 			$droppable = $game->data->allow_player_created_locations;
 		}
-		 $type = Media::getMediaType($fileName);
-        if($type == "Image"){
-			$iconNum = Module::kPLAYER_CREATED_ITEM_PHOTO_ICON_NUM;
+        
+        if($fileType != "NOTE")
+        {
+            //Create the Media
+            $newMediaResultData = Media::createMedia($gameId, $name, $fileName, 0);
+            $newMediaID = $newMediaResultData->data->media_id;
+		
+            $type = Media::getMediaType($fileName);
+            if($type == "Image"){
+                $iconNum = Module::kPLAYER_CREATED_ITEM_PHOTO_ICON_NUM;
+            }
+            else if($type == "Audio"){
+                $iconNum = Module::kPLAYER_CREATED_ITEM_AUDIO_ICON_NUM;
+            }
+            else if($type == "Video"){
+                $iconNum = Module::kPLAYER_CREATED_ITEM_VIDEO_ICON_NUM;
+            }
+            else{
+                $iconNum = Module::kPLAYER_CREATED_ITEM_DEFAULT_ICON_NUM;
+            }
         }
-        else if($type == "Audio"){
-			$iconNum = Module::kPLAYER_CREATED_ITEM_AUDIO_ICON_NUM;
+        else
+        {
+            $newMediaId = 0;
+            $iconNum = Module::kPLAYER_CREATED_ITEM_DEFAULT_ICON_NUM;
         }
-        else if($type == "Video"){
-           $iconNum = Module::kPLAYER_CREATED_ITEM_VIDEO_ICON_NUM;
-        }
-        else{
-		$iconNum = Module::kPLAYER_CREATED_ITEM_DEFAULT_ICON_NUM;
-		}
-		//Create the Item
-		$query = "INSERT INTO {$prefix}_items 
+        
+        //Create the Item
+        $query = "INSERT INTO {$prefix}_items 
 					(name, description, media_id, is_attribute, dropable, destroyable,
-					creator_player_id, origin_latitude, origin_longitude, icon_media_id)
+					creator_player_id, origin_latitude, origin_longitude, icon_media_id, fileType)
 					VALUES ('{$name}', 
 							'{$description}',
 							'{$newMediaID}', 
@@ -228,7 +238,7 @@ class Items extends Module
 							'$droppable',
 							'$destroyable',
 							'$playerId', '$latitude', '$longitude',
-							'$iconNum')";
+							'$iconNum', '$fileType')";
 		
 		NetDebug::trace("createItem: Running a query = $query");	
 		
