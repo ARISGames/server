f42c2215500307a1c548ad406e3333dca9d95335
Jacob Hanshaw <hanshaw@wisc.edu>
2012-08-31 21:13:20 +0000

    Edited moveables.php.

--- services/v1/movables.php
+++ services/v1/movables.php
@@ -11,93 +11,83 @@ class Movables extends Module
                                      game_id INT NOT NULL,
                                      type ENUM('Node', 'Item', 'Npc', 'WebPage', 'AugBubble', 'PlayerNote') NOT NULL,
                                      type_id INT NOT NULL,
-                                     location_name TINYTEXT NOT NULL DEFAULT '',
-                                     algorithm_type ENUM('TOWARD_PLAYER', 'STRAIGHT_LINE', 'CIRCLE', 'CUSTOM') NOT NULL DEFAULT 'TOWARD_PLAYER',
-                                     algorithm_detail INT NOT NULL DEFAULT 0,
+                                     algorithm_type ENUM('TOWARD_NEAREST_PLAYER', 'TOWARD_PLAYERS', 'STRAIGHT_LINE', 'CIRCLE', 'CUSTOM') NOT NULL DEFAULT 'TOWARD_NEAREST_PLAYER',
+                                     algorithm_detail_1 INT NOT NULL DEFAULT 0,
+                                     algorithm_detail_2 INT NOT NULL DEFAULT 0,
                                      velocity INT NOT NULL DEFAULT 0,
-                                     latitude DOUBLE NOT NULL default 0,
-                                     longitude DOUBLE NOT NULL default 0,
-                                     delete_when_viewed TINYINT(1) NOT NULL DEFAULT 0,
                                      move_stamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                     error_range INT NOT NULL DEFAULT 10,
-                                     force_view TINYINT(1) NOT NULL DEFAULT 0,
-                                     hidden TINYINT(1) NOT NULL DEFAULT 0,
-                                     allow_quick_travel TINYINT(1) NOT NULL DEFAULT 0,
-                                     wiggle TINYINT(1) NOT NULL DEFAULT 1,
-                                     active TINYINT(1) NOT NULL DEFAULT 1,
-                                     show_title TINYINT(1) NOT NULL DEFAULT 0,
-                                     time_to_live INT NOT NULL DEFAULT 100);";
+                                     active TINYINT(1) NOT NULL DEFAULT 1);";
                 mysql_query($query);
     }
  
-    public static function createMovable($gameId, $type, $typeId, $locationName, $algorithm_type, $algorithm_detail, $velocity, $moveStamp, $lat, $lon, $deleteWhenViewed, $timeToLive, $errorRange, $forceView, $hidden, $allowQuickTravel, $wiggle, $showTitle = 0)
+    public static function createMovable($gameId, $type, $typeId, $locationName, $algorithm_type, $algorithm_detail, $velocity, $moveStamp, $lat, $lon, $deleteWhenViewed, $errorRange, $forceView, $hidden, $allowQuickTravel, $wiggle, $showTitle = 0)
     {
-        if($spawnableId = Spawnables::hasSpawnable($gameId, $type, $typeId))
-            $query = "UPDATE spawnables SET active = 1 WHERE game_id = $gameId AND type = '$type' AND type_id = $typeId";
+        if($movableId = Movables::hasMovable($gameId, $type, $typeId))
+            $query = "UPDATE movables SET active = 1 WHERE game_id = $gameId AND type = '$type' AND type_id = $typeId";
         else
-            $query = "INSERT INTO spawnables (game_id, type, type_id, location_name, amount, min_area, max_area, amount_restriction, location_bound_type, latitude, longitude, spawn_probability, spawn_rate, delete_when_viewed, time_to_live, error_range, force_view, hidden, allow_quick_travel, wiggle, show_title, active) VALUES ($gameId, '{$type}', $typeId, '$locationName', $amount, $minArea, $maxArea, '{$amountRestriction}', '{$locationBoundType}', $lat, $lon, $spawnProbability, $spawnRate, $deleteWhenViewed, $timeToLive, $errorRange, $forceView, $hidden, $allowQuickTravel, $wiggle, $showTitle, 1);";
+            $query = "INSERT INTO movables (game_id, type, type_id, location_name, algorithm_type, algorithm_detail, velocity, latitude, longitude, delete_when_viewed, error_range, force_view, hidden, allow_quick_travel, wiggle, show_title, active) VALUES ($gameId, '{$type}', $typeId, '$locationName', '{$algorithm_type}', $algorithm_detail, $velocity, $lat, $lon, $deleteWhenViewed, $errorRange, $forceView, $hidden, $allowQuickTravel, $wiggle, $showTitle, 1);";
 
         mysql_query($query);
-        $spawnableId = mysql_insert_id();
-        return new returnData(0,$spawnableId);
+        $movableId = mysql_insert_id();
+        return new returnData(0,$movableId);
     }
 
-    public static function hasActiveSpawnable($gameId, $type, $typeId)
+    public static function hasActiveMovable($gameId, $type, $typeId)
     {
-        $query = "SELECT * FROM spawnables WHERE game_id = $gameId AND type = '$type' AND type_id = $typeId AND active = 1"; 
+        $query = "SELECT * FROM movables WHERE game_id = $gameId AND type = '$type' AND type_id = $typeId AND active = 1"; 
         $result = mysql_query($query);
-        if($obj = mysql_fetch_object($result)) return $obj->spawnable_id;
+        if($obj = mysql_fetch_object($result)) return $obj->movable_id;
         else return false;
     }
 
-    public static function hasSpawnable($gameId, $type, $typeId)
+    public static function hasMovable($gameId, $type, $typeId)
     {
-        $query = "SELECT * FROM spawnables WHERE game_id = $gameId AND type = '$type' AND type_id = $typeId"; 
+        $query = "SELECT * FROM movables WHERE game_id = $gameId AND type = '$type' AND type_id = $typeId"; 
         $result = mysql_query($query);
-        if($obj = mysql_fetch_object($result)) return $obj->spawnable_id;
+        if($obj = mysql_fetch_object($result)) return $obj->movable_id;
         else return false;
     }
 
-    public static function deleteSpawnable($spawnableId)
+    public static function deleteMovable($movableId)
     {
-        $query = "UPDATE spawnables SET active = 0 WHERE spawnable_id = $spawnableId";
+        $query = "UPDATE movables SET active = 0 WHERE movable_id = $movableId";
         mysql_query($query);
         /*
         //This does a hard delete
-        $query = "SELECT * FROM spawnables WHERE spawnable_id = $spawnableId";
+        $query = "SELECT * FROM movables WHERE movable_id = $movableId";
         $result = mysql_query($query);
         $obj = mysql_fetch_object($result);
         if($obj)
         {
-        $query = "DELETE FROM spawnables WHERE spawnable_id = $spawnableId";
+        $query = "DELETE FROM movables WHERE movable_id = $movableId";
         mysql_query($query);
-        $query = "DELETE FROM ".$obj->game_id."_requirements WHERE content_type = 'Spawnable' AND content_id = $spawnableId";
+        $query = "DELETE FROM ".$obj->game_id."_requirements WHERE content_type = 'Movable' AND content_id = $movableId";
         mysql_query($query);
         }
          */
         return new returnData(0);
     }
 
-    public static function deleteSpawnablesOfObject($gameId, $type, $typeId)
+    public static function deleteMovablesOfObject($gameId, $type, $typeId)
     {
 
-        if($spawnableId = Spawnables::hasSpawnable($gameId, $type, $typeId))
-            Spawnables::deleteSpawnable($spawnableId);
+        if($movableId = Movables::hasMovable($gameId, $type, $typeId))
+            Movables::deleteMovable($movableId);
         return new returnData(0);
     }
 
-    //Optionally by spawnableId or by gameId, type, and typeId
-    public static function updateSpawnable($spawnableId = 0, $gameId, $type, $typeId, $locationName, $amount, $minArea, $maxArea, $amountRestriction, $locationBoundType, $lat, $lon, $spawnProbability, $spawnRate, $deleteWhenViewed, $timeToLive, $errorRange, $forceView, $hidden, $allowQuickTravel, $wiggle, $active, $showTitle)
+    //Optionally by movableId or by gameId, type, and typeId
+    public static function updateMovable($movableId = 0, $gameId, $type, $typeId, $locationName, $amount, $minArea, $maxArea, $amountRestriction, $locationBoundType, $lat, $lon, $spawnProbability, $spawnRate, $deleteWhenViewed, $timeToLive, $errorRange, $forceView, $hidden, $allowQuickTravel, $wiggle, $active, $showTitle)
     {
-        if($spawnableId == 0)
-            $query = "UPDATE spawnables SET location_name = '$locationName', amount = $amount, min_area = $minArea, max_area = $maxArea, amount_restriction = '{$amountRestriction}', location_bound_type = '{$locationBoundType}', latitude = $lat, longitude = $lon, spawn_probability = $spawnProbability, spawn_rate = $spawnRate, delete_when_viewed = $deleteWhenViewed, time_to_live = $timeToLive, error_range = $errorRange, force_view = $forceView, hidden = $hidden, allow_quick_travel = $allowQuickTravel, wiggle = $wiggle, show_title = $showTitle, active = $active WHERE game_id = $gameId AND type = '{$type}' AND type_id = $typeId";
+        if($movableId == 0)
+            $query = "UPDATE movables SET location_name = '$locationName', amount = $amount, min_area = $minArea, max_area = $maxArea, amount_restriction = '{$amountRestriction}', location_bound_type = '{$locationBoundType}', latitude = $lat, longitude = $lon, spawn_probability = $spawnProbability, spawn_rate = $spawnRate, delete_when_viewed = $deleteWhenViewed, time_to_live = $timeToLive, error_range = $errorRange, force_view = $forceView, hidden = $hidden, allow_quick_travel = $allowQuickTravel, wiggle = $wiggle, show_title = $showTitle, active = $active WHERE game_id = $gameId AND type = '{$type}' AND type_id = $typeId";
         else
-            $query = "UPDATE spawnables SET game_id = $gameId, type = '$type', type_id = $typeId, location_name = '$locationName', amount = $amount, min_area = $minArea, max_area = $maxArea, amount_restriction = '{$amountRestriction}', location_bound_type = '{$locationBoundType}', latitude = $lat, longitude = $lon, spawn_probability = $spawnProbability, spawn_rate = $spawnRate, delete_when_viewed = $deleteWhenViewed, time_to_live = $timeToLive, error_range = $errorRange, force_view = $forceView, hidden = $hidden, allow_quick_travel = $allowQuickTravel, wiggle = $wiggle, show_title = $showTitle, active = $active WHERE spawnable_id = $spawnableId";
+            $query = "UPDATE movables SET game_id = $gameId, type = '$type', type_id = $typeId, location_name = '$locationName', amount = $amount, min_area = $minArea, max_area = $maxArea, amount_restriction = '{$amountRestriction}', location_bound_type = '{$locationBoundType}', latitude = $lat, longitude = $lon, spawn_probability = $spawnProbability, spawn_rate = $spawnRate, delete_when_viewed = $deleteWhenViewed, time_to_live = $timeToLive, error_range = $errorRange, force_view = $forceView, hidden = $hidden, allow_quick_travel = $allowQuickTravel, wiggle = $wiggle, show_title = $showTitle, active = $active WHERE movable_id = $movableId";
         mysql_query($query);
         return new returnData(0);
     }
 
-    public static function createSpawnableForObject($gameId, $type, $typeId)
+    public static function createMovableForObject($gameId, $type, $typeId)
     {
         switch ($type) {
             case 'Item':
@@ -119,28 +109,28 @@ class Movables extends Module
         $result = mysql_query($query);
         $obj = mysql_fetch_object($result);
         $title = $obj->title;
-        Spawnables::createSpawnable($gameId, $type, $typeId, $title, 5, 35, 50, 'PER_PLAYER', 'PLAYER', 0, 0, 50, 10, 0, 100, 15, 0, 0, 0, 1, 0);
-        return Spawnables::getSpawnableForObject($gameId, $type, $typeId);
+        Movables::createMovable($gameId, $type, $typeId, $title, 5, 35, 50, 'PER_PLAYER', 'PLAYER', 0, 0, 50, 10, 0, 100, 15, 0, 0, 0, 1, 0);
+        return Movables::getMovableForObject($gameId, $type, $typeId);
     }
 
-    public static function getSpawnableForObject($gameId, $type, $typeId)
+    public static function getMovableForObject($gameId, $type, $typeId)
     {
-        $query = "SELECT * FROM spawnables WHERE game_id = $gameId AND type = '".$type."' AND type_id = '".$typeId."' AND active = 1 LIMIT 1";
+        $query = "SELECT * FROM movables WHERE game_id = $gameId AND type = '".$type."' AND type_id = '".$typeId."' AND active = 1 LIMIT 1";
         $result = mysql_query($query);
         $obj = mysql_fetch_object($result);
         if($obj) return new returnData(0, $obj);
-        else return new returnData(1, "No Spawnables For Object");
+        else return new returnData(1, "No Movables For Object");
     }
 
-    public static function getSpawnablesForGame($gameId)
+    public static function getMovablesForGame($gameId)
     {
-        $query = "SELECT * FROM spawnables WHERE game_id = $gameId AND active = 1";
+        $query = "SELECT * FROM movables WHERE game_id = $gameId AND active = 1";
         $result = mysql_query($query);
-        $spawnables = array();
+        $movables = array();
         while($obj = mysql_fetch_object($result))
         {
-            $spawnables[] = $obj;
+            $movables[] = $obj;
         }
-        return new returnData(0, $spawnables);
+        return new returnData(0, $movables);
     }
 }
