5c8cad1537a99a5b5a478dd76befa80d96e0f588
phildo <Phildo211@gmail.com>
2012-11-13 19:21:28 +0000

    fixed some authentication stuff

--- events/auths/presence_auth.php
+++ events/auths/presence_auth.php
@@ -5,14 +5,13 @@ require_once('../pusher_defaults.php');
 
 $pusher = new Pusher(Config::pusher_key, Config::pusher_secret, Config::pusher_app_id, true);
 
-$channel = isset($_REQUEST['channel']) ? $_REQUEST['channel'] : $presence_default_channel;
+$channel = isset($_REQUEST['channel_name']) ? $_REQUEST['channel_name'] : $presence_default_channel;
 $socket_id = $_REQUEST['socket_id'];
 
 $user_id = (isset($_SESSION['user_id']) ? $_SESSION['user_id'] : time());
 $_SESSION['user_id'] = $user_id;
 
-$user_name = isset($_REQUEST['user_name']) ? $_REQUEST['user_name'] : 'valued_voter';
+$user_name = isset($_REQUEST['user_name']) ? $_REQUEST['user_name'] : 'valued_user';
 
 echo $pusher->presence_auth($channel, $socket_id, $user_id, array('name' => $user_name));
-
 ?>
--- events/auths/private_auth.php
+++ events/auths/private_auth.php
@@ -3,10 +3,9 @@ require_once('../../config.class.php');
 require_once('../Pusher.php');
 require_once('../pusher_defaults.php');
 
-$channel = isset($_REQUEST['channel']) ? $_REQUEST['channel'] : $private_default_channel;
+$channel = isset($_REQUEST['channel_name']) ? $_REQUEST['channel_name'] : $private_default_channel;
 $socket_id = $_REQUEST['socket_id'];
 
 $pusher = new Pusher(Config::pusher_key, Config::pusher_secret, Config::pusher_app_id, true);
 echo $pusher->socket_auth($channel, $socket_id);
-    
 ?>
