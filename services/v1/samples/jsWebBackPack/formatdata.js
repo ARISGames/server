0c450d753ebf5aa8b754afb43466585e3b131d32
phildo <Phildo211@gmail.com>
2012-12-03 22:53:33 +0000

    merged in updated web backpack to account for display name and image

--- services/v1/samples/jsWebBackPack/formatdata.js
+++ services/v1/samples/jsWebBackPack/formatdata.js
@@ -81,10 +81,16 @@ function formatPage(game)
             add("<div class='player'>\n");
             add("<div class='playerheader'>\n");
             add("<div class='playericon'>\n");
-            add(format_img('profpic.png', '', 'index.html?mode=web&gameId='+game.game_id+'&playerId='+bp.owner.player_id, 'playericonimage'));
+            if(bp.owner.player_pic_url && bp.owner.player_pic_url != "")
+                add(format_img(bp.owner.player_pic_url, '', 'index.html?mode=web&gameId='+game.game_id+'&playerId='+bp.owner.player_id, 'playericonimage'));
+            else
+                add(format_img('profpic.png', '', 'index.html?mode=web&gameId='+game.game_id+'&playerId='+bp.owner.player_id, 'playericonimage'));
             add("</div>\n");//<- class 'playericon'
             add("<div class='playertext'>\n");
-            add("<div class='playername'>"+bp.owner.user_name+"</div>\n");
+            if(bp.owner.display_name && bp.owner.display_name != "")
+                add("<div class='playername'>"+bp.owner.display_name+"</div>\n");
+            else
+                add("<div class='playername'>"+bp.owner.user_name+"</div>\n");
             add("</div>\n");//<- class 'playertext'
             add("</div>\n");//<- class 'playerheader'
 
@@ -104,17 +110,6 @@ function formatPage(game)
                     add("<div class='spacer sectionspacer'> </div>\n");
                 else
                     firstSection=false;
-
-            //Output player header
-            add("<div class='player'>\n");
-            add("<div class='playerheader'>\n");
-            add("<div class='playericon'>\n");
-            add(format_img('profpic.png', '', 'index.html?mode=web&gameId='+game.game_id+'&playerId='+bp.owner.player_id, 'playericonimage'));
-            add("</div>\n");//<- class 'playericon'
-            add("<div class='playertext'>\n");
-            add("<div class='playername'>"+bp.owner.user_name+"</div>\n");
-            add("</div>\n");//<- class 'playertext'
-            add("</div>\n");//<- class 'playerheader'
 
                 firstContent = true;
                 add("<div class='sectionheader'>Attributes</div>\n");
