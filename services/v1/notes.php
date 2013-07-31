de15e24398f3e9c0f0ead6f737fbfd10d09b0f28
hanshaw <hanshaw@wisc.edu>
2013-07-31 17:12:23 -0500

    Commented Out Error Checking Code

--- services/v1/notes.php
+++ services/v1/notes.php
@@ -1,7 +1,7 @@
 <?php
 
-ini_set('display_errors', 'On');
-error_reporting(E_ALL);
+//ini_set('display_errors', 'On');
+//error_reporting(E_ALL);
 
 require_once("module.php");
 require_once("media.php");
--- sync/index.php
+++ sync/index.php
@@ -1,6 +1,6 @@
 <?php
-ini_set('display_errors', 'On');
-error_reporting(E_ALL);
+//ini_set('display_errors', 'On');
+//error_reporting(E_ALL);
 $version = "v1";
 
 require_once("actions.php");
