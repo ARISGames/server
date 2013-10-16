2d026c1b1ca3eee35cda25c16118399f0cae52aa
Phildo <phildo211@gmail.com>
2013-10-16 14:49:40 -0500

    better quest saving

--- services/v1/quests.php
+++ services/v1/quests.php
@@ -55,18 +55,16 @@ class Quests extends Module
         return new returnData(0, $event);
     }
 
-    public function createQuest($gameId, $strName, $strIncompleteDescription, $strCompleteDescription, $boolFullScreenNotification, $intActiveMediaId, $intCompleteMediaId, $intActiveIconMediaId, $intCompleteIconMediaId, $goFunction, $index, $editorId, $editorToken)
+    public function createQuest($gameId, $strName, $index, $editorId, $editorToken)
     {
         if(!Module::authenticateGameEditor($gameId, $editorId, $editorToken, "read_write"))
             return new returnData(6, NULL, "Failed Authentication");
 
         $strName = addslashes($strName);	
-        $strIncompleteDescription = addslashes($strIncompleteDescription);	
-        $strCompleteDescription = addslashes($strCompleteDescription);	
 
         $query = "INSERT INTO quests 
-            (game_id, name, description, text_when_complete, sort_index, go_function, full_screen_notify)
-            VALUES ('{$gameId}','{$strName}','{$strIncompleteDescription}','{$strCompleteDescription}','{$index}','{$goFunction}','{$boolFullScreenNotification}')";
+            (game_id, name, sort_index)
+            VALUES ('{$gameId}','{$strName}','{$index}')";
 
 
         Module::query($query);
@@ -75,38 +73,51 @@ class Quests extends Module
         return new returnData(0, mysql_insert_id());
     }
 
-    public function updateQuest($gameId, $intQuestId, $strName, $strIncompleteDescription, $strCompleteDescription, $boolFullScreenNotification, $intActiveMediaId, $intCompleteMediaId, $intActiveIconMediaId, $intCompleteIconMediaId, $goFunction, $index, $editorId, $editorToken)
+    public function updateQuest($gameId, $questId,
+        $name,
+        $activeText, $completeText, 
+        $activeNotifText, $completeNotifText,
+        $activeMediaId, $completeMediaId, 
+        $activeIconMediaId, $completeIconMediaId, 
+        $activeNotifMediaId, $completeNotifMediaId,
+        $activeFunction, $completeFunction,
+        $activeNotifFunction, $completeNotifFunction, 
+        $activeFullScreenNotify, $completeFullScreenNotify,
+        $activeShowDismiss, $completeShowDismiss
+        $sortIndex,
+        $editorId, $editorToken)
     {
         if(!Module::authenticateGameEditor($gameId, $editorId, $editorToken, "read_write"))
             return new returnData(6, NULL, "Failed Authentication");
 
-        $strName = addslashes($strName);	
-        $strIncompleteDescription = addslashes($strIncompleteDescription);	
-        $strCompleteDescription = addslashes($strCompleteDescription);	
+        Module::query("UPDATE quests SET
+            name                            = '"+addslashes($name)+"',
+            description                     = '"+addslashes($activeText)+"',
+            text_when_complete              = '"+addslashes($completeText)+"',
+            description_notification        = '"+addslashes($activeNotifText)+"',
+            text_when_complete_notification = '"+addslashes($completeNotifText)+"',
+            active_media_id                 = '"+intval($activeMediaId)+"',
+            complete_media_id               = '"+intval($completeMediaId)+"',
+            active_icon_media_id            = '"+intval($activeIconMediaId)+"',
+            complete_icon_media_id          = '"+intval($completeIconMediaId)+"',
+            active_notification_media_id    = '"+intval($activeNotifMediaId)+"',
+            complete_notification_media_id  = '"+intval($completeNotifMediaId)+"',
+            go_function                     = '"+addslashes($activeFunction)+"',
+            complete_go_function            = '"+addslashes($completeFunction)+"',
+            notif_go_function               = '"+addslashes($activeNotifFunction)+"',
+            complete_notif_go_function      = '"+addslashes($completeNotifFunction)+"',
+            full_screen_notify              = '"+intval($activeFullScreenNotify)+"',
+            complete_full_screen_notify     = '"+intval($completeFullScreenNotify)+"',
+            active_notif_show_dismiss       = '"+intval($activeShowDismiss)+"',
+            complete_notif_show_dismiss     = '"+intval($completeShowDismiss)+"',
+            sort_index                      = '"+intval($sortIndex)+"'
+            WHERE
+            game_id = '"+intval($gameId)+"' AND quest_id = '"+$questId+"';");
 
-        $query = "UPDATE quests 
-            SET 
-            name = '{$strName}',
-                 description = '{$strIncompleteDescription}',
-                 text_when_complete = '{$strCompleteDescription}',
-                 sort_index = '{$index}',
-                 full_screen_notify = '{$boolFullScreenNotification}'
-                     WHERE game_id = {$gameId} AND quest_id = '{$intQuestId}'";
+        if(mysql_error()) return new returnData(3, NULL, "SQL Error");
 
-                 //removed from query until fully implemented
-                 //go_function = '{$goFunction}',
-                 //active_media_id = '{$intActiveMediaId}',
-                 //complete_media_id = '{$intCompleteMediaId}',
-                 //active_icon_media_id = '{$intActiveIconMediaId}',
-                 //complete_icon_media_id = '{$intCompleteIconMediaId}',
-
-        Module::query($query);
-
-        if (mysql_error()) return new returnData(3, NULL, "SQL Error");
-
-        if (mysql_affected_rows()) return new returnData(0, TRUE);
+        if(mysql_affected_rows()) return new returnData(0, TRUE);
         else return new returnData(0, FALSE);
-
     }
 
     public function deleteQuest($gameId, $intQuestId, $editorId, $editorToken)
