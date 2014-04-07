301eec9acd3818f15a5b419fc573a9a3b6ae82e0
Phildo <phildo211@gmail.com>
2014-04-07 15:33:20 -0500

    hopefully massive speedup to game reset

--- services/v1/players.php
+++ services/v1/players.php
@@ -209,8 +209,8 @@ class Players extends Module
 
 	public function startOverGameForPlayer($gameId, $playerId)
 	{	
-		Module::query("DELETE FROM player_items WHERE player_id = '{$playerId}' AND game_id = {$gameId}");
-		Module::query("UPDATE player_log SET deleted = 1 WHERE player_id = '{$playerId}' AND game_id = '{$gameId}'");
+		Module::query("DELETE FROM player_items WHERE game_id = {$gameId} AND player_id = '{$playerId}'");
+		Module::query("UPDATE player_log SET deleted = 1 WHERE game_id = '{$gameId}' AND player_id = '{$playerId}'");
 
 		return new returnData(0, TRUE);
 	}	
