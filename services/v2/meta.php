6f03877b852e19081015b3f76ba28f029b9f0fa0
Phildo <phildo211@gmail.com>
2014-04-28 16:00:41 -0500

    cleanup of legacy stuff

--- services/v2/meta.php
+++ services/v2/meta.php
@@ -531,5 +531,163 @@ class meta extends dbconnection
         return $contents;
     }
 
+    public function searchGameForErrors($gid){
+
+        //     $query = "SELECT name FROM games WHERE game_id = {$gid}";
+        //     $name = Module::query($query);
+
+        //return "\nLooking for problems in {$name}\nNote: This check does not quarantee there are no errors in your game, but only checks for a few common mistakes.\n";	
+
+        $query = "SELECT * FROM requirements WHERE game_id = {$gid}";
+        $resultMain = Module::query($query);
+        while($resultMain && $row = mysql_fetch_object($resultMain)){ 
+            if(!$row->requirement_detail_1){
+                if($row->requirement == "PLAYER_HAS_ITEM" || $row->requirement == "PLAYER_VIEWED_ITEM"){
+                    if(!($row->content_type == "Node")) return "\nThere is a requirement of a {$row->content_type} with id: {$row->content_id} that doesn't specify which item the player needs to have/have viewed.\n";	
+                    else{
+                        $scriptTitle = Module::query("SELECT title FROM nodes WHERE game_id = {$gid} AND node_id = {$row->content_id}");
+                        return "\nThere is a requirement of a {$row->content_type} with the title of {$scriptTitle} that doesn't specify which item the player needs to have/have viewed.\n";	
+                    }
+                    if(!$row->requirement_detail_2 && $row->requirement == "Player_HAS_ITEM"){
+                        if(!($row->content_type == "Node")) return "\nThere is a requirement of a {$row->content_type} with id: {$row->content_id} that requires the player has a certain item, but not the quantity of that item needed.\n";	
+                        else{
+                            $scriptTitle = Module::query("SELECT title FROM nodes WHERE game_id = {$gid} AND node_id = {$row->content_id}");
+                            return "\nThere is a requirement of a {$row->content_type} with the title of {$scriptTitle} that requires that the player has a certain item, but not the quantity of that item neeeded.\n";	
+                        }
+                    }
+                } 
+                else if($row->requirement == "PLAYER_VIEWED_NODE"){
+                    if(!($row->content_type == "Node")) return "\nThere is a requirement of a {$row->content_type} with id: {$row->content_id} that doesn't specify which node the player needed to view in order to satisfy that requirement.\n";	
+                    else{
+                        $scriptTitle = Module::query("SELECT title FROM nodes WHERE game_id = {$gid} AND node_id = {$row->content_id}");
+                        return "\nThere is a requirement of a {$row->content_type} with the title of  {$scriptTitle} that doesn't specify which node the player needed to view in order to satisfy that requirement.\n";	
+                    }
+                }
+                else if($row->requirement == "PLAYER_VIEWED_NPC"){
+                    if(!($row->content_type == "Node")) return "\nThere is a requirement of a {$row->content_type} with id: {$row->content_id} that doesn't specify which character the player needed to view in order to satisfy that requirement.\n";	
+                    else{
+                        $scriptTitle = Module::query("SELECT title FROM nodes WHERE game_id = {$gid} AND node_id = {$row->content_id}");
+                        return "\nThere is a requirement of a {$row->content_type} with the title of {$scriptTitle} that doesn't specify which character the player needed to view in order to satisfy that requirement.\n";	
+                    }
+                }
+                else if($row->requirement == "PLAYER_VIEWED_WEBPAGE"){
+                    if(!($row->content_type == "Node")) return "\nThere is a requirement of a {$row->content_type} with id: {$row->content_id} that doesn't specify which web page the player needed to view in order to satisfy that requirement.\n";	
+                    else{
+                        $scriptTitle = Module::query("SELECT title FROM nodes WHERE game_id = {$gid} AND node_id = {$row->content_id}");
+                        return "\nThere is a requirement of a {$row->content_type} with the title of {$scriptTitle} that doesn't specify which web page the player needed to view in order to satisfy that requirement.\n";	
+                    }
+
+                }
+                else if($row->requirement == "PLAYER_VIEWED_AUGBUBBLE"){
+                    if(!($row->content_type == "Node")) return "\nThere is a requirement of a {$row->content_type} with id: {$row->content_id} that doesn't specify which panoramic the player needed to view in order to satisfy that requirement.\n";
+                    else{
+                        $scriptTitle = Module::query("SELECT title FROM nodes WHERE game_id = {$gid} AND node_id = {$row->content_id}");
+                        return "\nThere is a requirement of a {$row->content_type} with the title of {$scriptTitle} that doesn't specify which panoramic the player needed to view in order to satisfy that requirement.\n";
+                    }
+                }
+                else if($row->requirement == "PLAYER_HAS_COMPLETED_QUEST"){
+                    if(!($row->content_type == "Node")) return "\nThere is a requirement of a {$row->content_type} with id: {$row->content_id} that doesn't specify which quest the player needed to complete in order to satisfy that requirement.\n";	
+                    else{
+                        $scriptTitle = Module::query("SELECT title FROM {$gid}_nodes WHERE node_id = {$row->content_id}");
+                        return "\nThere is a requirement of a {$row->content_type} with the title of {$scriptTitle} that doesn't specify which quest the player needed to complete in order to satisfy that requirement.\n";	
+                    }
+                }
+                else if($row->requirement == "PLAYER_HAS_RECEIVED_INCOMING_WEB_HOOK"){
+                    if(!($row->content_type == "Node")) return "\nThere is a requirement of a {$row->content_type} with id: {$row->content_id} that doesn't specify which incoming web hook the player needed to receive in order to satisfy that requirement.\n";	
+                    else{
+                        $scriptTitle = Module::query("SELECT title FROM {$gid}_nodes WHERE node_id = {$row->content_id}");
+                        return "\nThere is a requirement of a {$row->content_type} with the title of {$scriptTitle} that doesn't specify which incoming web hook the player needed to receive in order to satisfy that requirement.\n";	
+                    }
+
+                } 
+            }
+        }
+
+        $query = "SELECT * FROM player_state_changes";
+        $resultMain = Module::query($query);
+        while($resultMain && $row = mysql_fetch_object($resultMain)){ 
+            if($row->event_type == "VIEW_ITEM"){
+                if(!$row->action_detail){
+                    return "\nThere is an item of id: {$row->event_detail} that doesn't specify what item to give or take when viewed.\n";	
+                }
+                if(!$row->action_amount){
+                    return "\nThere is an item of id: {$row->event_detail} that doesn't specify what quantity of an item to give or take when viewed.\n";	
+                }
+            }
+            else if($row->event_type == "VIEW_NODE"){
+                if(!$row->action_detail){
+                    $scriptTitle = Module::query("SELECT title FROM nodes WHERE game_id = {$gid} AND node_id = {$row->event_detail}");
+                    return "\nThere is a node with the title of {$scriptTitle} that doesn't specify what item to give or take when viewed.\n";	
+                }
+                if(!$row->action_amount){
+                    $scriptTitle = Module::query("SELECT title FROM nodes WHERE game_id = {$gid} AND node_id = {$row->event_detail}");
+                    return "\nThere is a node with the title of {$scriptTitle} that doesn't specify what quantity of an item to give or take when viewed.\n";	
+                }
+            }
+            else if($row->event_type == "VIEW_NPC"){
+                if(!$row->action_detail){
+                    return "\nThere is a character of id: {$row->event_detail} that doesn't specify what item to give or take when viewed.\n";	
+                }
+                if(!$row->action_amount){
+                    return "\nThere is a character of id: {$row->event_detail} that doesn't specify what quantity of an item to give or take when viewed.\n";	
+                }
+            } 
+            else if($row->event_type == "VIEW_WEBPAGE"){
+                if(!$row->action_detail){
+                    return "\nThere is a web page of id: {$row->event_detail} that doesn't specify what item to give or take when viewed.\n";	
+                }
+                if(!$row->action_amount){
+                    return "\nThere is a web page of id: {$row->event_detail} that doesn't specify what quantity of an item to give or take when viewed.\n";	
+                }
+            } 
+            else if($row->event_type == "VIEW_AUGBUBBLE"){
+                if(!$row->action_detail){
+                    return "\nThere is a panoramic of id: {$row->event_detail} that doesn't specify what item to give or take when viewed.\n";	
+                }
+                if(!$row->action_amount){
+                    return "\nThere is a panoramic of id: {$row->event_detail} that doesn't specify what quantity of an item to give or take when viewed.\n";	
+                }
+            } 
+            else if($row->event_type == "RECEIVE_WEBHOOK"){
+                if(!$row->action_detail){
+                    return "\nThere is an web hook of id: {$row->event_detail} that doesn't specify what item to give or take when received.\n";	
+                }
+                if(!$row->action_amount){
+                    return "\nThere is an web hook of id: {$row->event_detail} that doesn't specify what quantity of an item to give or take when received.\n";	
+                }
+            }
+        }
+
+        $query = "SELECT * FROM nodes WHERE game_id = {$gid}";
+        $result = Module::query($query);
+        while($result && $row = mysql_fetch_object($result)) {
+            if($row->text){
+                $inputString = $row->text;
+                if((strspn($inputString,"<>") > 0) && ((substr_count($inputString, "<npc>") > 0) || (substr_count($inputString, "<pc>") > 0) || (substr_count($inputString, "<dialog>") > 0)) && !(substr_count($inputString,"<p>") > 0) && !(substr_count($inputString,"<b>") > 0) && !(substr_count($inputString,"<i>") > 0) && !(substr_count($inputString,"<img") > 0) && !(substr_count($inputString,"<table>") > 0)){
+                    @$output = simplexml_load_string($inputString);
+                    if(!$output) return "\nThere is improperly formatted xml in the node with title:\n{$row->title}\nand text:\n{$row->text}\n";
+                }
+            }
+        }
+
+        $query = "SELECT * FROM npcs WHERE game_id = {$gid}";
+        $result = Module::query($query);
+        while($result && $row = mysql_fetch_object($result)) {
+            if($row->text){
+                $inputString = $row->text;
+                if((strspn($inputString,"<>") > 0) && ((substr_count($inputString, "<npc>") > 0) || (substr_count($inputString, "<pc>") > 0) || (substr_count($inputString, "<dialog>") > 0)) && !(substr_count($inputString,"<p>") > 0) && !(substr_count($inputString,"<b>") > 0) && !(substr_count($inputString,"<i>") > 0) && !(substr_count($inputString,"<img") > 0) && !(substr_count($inputString,"<table>") > 0)){
+                    @$output = simplexml_load_string($inputString);
+                    if(!$output) return "\nThere is improperly formatted xml in the npc with name:\n{$row->name}\nand greeting:\n{$row->text}\n";
+                }
+            }
+            if($row->closing){
+                $inputString = $row->closing;
+                if((strspn($inputString,"<>") > 0) && ((substr_count($inputString, "<npc>") > 0) || (substr_count($inputString, "<pc>") > 0) || (substr_count($inputString, "<dialog>") > 0)) && !(substr_count($inputString,"<p>") > 0) && !(substr_count($inputString,"<b>") > 0) && !(substr_count($inputString,"<i>") > 0) && !(substr_count($inputString,"<img") > 0) && !(substr_count($inputString,"<table>") > 0)){
+                    @$output = simplexml_load_string($inputString);
+                    if(!$output) return "\nThere is improperly formatted xml in the npc with name:\n{$row->name}\nand closing:\n{$row->text}\n";
+                }
+            }
+        }  
+    }
 }
 ?>
