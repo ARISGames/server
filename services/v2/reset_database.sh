80d311b078a35757fbc1f60ddbb8ecd8338e7328
Phildo <phildo211@gmail.com>
2014-06-16 11:47:25 -0500

    updated player log sql

--- services/v2/reset_database.sh
+++ services/v2/reset_database.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cat reset_database.sql | tee | mysql -u root -p
-rm -rf /var/www/html/server/gamedata/*
+sudo rm -rf /var/www/html/server/gamedata/*
 git checkout /var/www/html/server/gamedata
