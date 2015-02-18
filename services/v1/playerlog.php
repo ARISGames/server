9832f6adbe860fbca87406ce895219c2f9445217
Phildo <phildo211@gmail.com>
2015-02-18 11:26:14 -0600

    segmented player log get allows for massive requests without blowing memory

--- services/v1/playerlog.php
+++ services/v1/playerlog.php
@@ -6,15 +6,11 @@ class PlayerLog extends Module
 {
     public function getPlayerLogs($glob)
     {
+	ini_set('display_errors',1);  error_reporting(E_ALL);
         //Grrr amfphp should take care of this...
 	$data = file_get_contents("php://input");
         $glob = json_decode($data);
 
-        return PlayerLog::getPlayerLogsGlob($glob);
-    }
-
-    public function getPlayerLogsGlob($glob)
-    {
         $reqOutputFormat = $glob->output_format;
 
         $reqGameId      = $glob->game_id;
@@ -30,10 +26,6 @@ class PlayerLog extends Module
 
         $reqGetExpired = $glob->get_expired;
         $reqVerbose    = $glob->verbose;
-
-        //for segmented csv creation
-        $includeHeader = $glob->include_header;
-        $append        = $glob->append;
 
         //validation
         $expectsNotice = 'Expects JSON argument of minimal form: {"output_format":"json","game_id":1,"editor_id":1,"editor_token":"abc123"}';
@@ -58,8 +50,6 @@ class PlayerLog extends Module
         if(!preg_match("/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/",$reqEndDate))   $reqEndDate   = "9999-00-00 00:00:00";
         if(!is_numeric($reqGetExpired)) $reqGetExpired = 0; else if(intval($reqGetExpired) > 0) $reqGetExpired = 1;
         if(!is_numeric($reqVerbose))    $reqVerbose    = 0; else if(intval($reqVerbose)    > 0) $reqVerbose    = 1;
-        if(!is_numeric($includeHeader)) $includeHeader = 1; else if(intval($includeHeader) > 0) $includeHeader = 1; else $includeHeader = 0; //default 1
-        if(!is_numeric($append))        $append        = 0; else if(intval($append)        > 0) $append        = 1;
 
         $playerLogs = array();
         if($filterMode == "group")
@@ -128,201 +118,187 @@ class PlayerLog extends Module
         $webhooksA = Module::queryArray("SELECT web_hook_id, name FROM web_hooks WHERE game_id = '{$reqGameId}'");
         $webhooksH = array(); for($i = 0; $i < count($webhooksA); $i++) $webhooksH[$webhooksA[$i]->web_hook_id] = $webhooksA[$i];
 
-        for($i = 0; $i < count($playerLogs); $i++)
-        {
-            $playerLogs[$i]->log = array();
-	    $r = Module::queryArray("SELECT * FROM player_log WHERE player_id = '{$playerLogs[$i]->player->player_id}' AND game_id = '{$reqGameId}' AND  timestamp BETWEEN '{$reqStartDate}' AND '{$reqEndDate}' AND (deleted = 0 OR deleted = {$reqGetExpired})");
-            for($j = 0; $j < count($r); $j++)
-            {
-                $row = new stdClass();
-                switch($r[$j]->event_type)
-                {
-                    case "PICKUP_ITEM":
-                        $row->event = "Received Item";
-                        $row->object = $itemsH[$r[$j]->event_detail_1]->name;
-                        $row->qty = $r[$j]->event_detail_2;
-                        $row->timestamp = $r[$j]->timestamp;
-                        $row->human = $playerLogs[$i]->player->display_name." received ".$row->qty." ".$row->object." (Item).";
-                        break;
-                    case "DROP_ITEM":
-                    case "DESTROY_ITEM":
-                        $row->event = "Lost Item";
-                        $row->object = $itemsH[$r[$j]->event_detail_1]->name;
-                        $row->qty = $r[$j]->event_detail_2;
-                        $row->timestamp = $r[$j]->timestamp;
-                        $row->human = $playerLogs[$i]->player->display_name." lost ".$row->qty." ".$row->object." (Item).";
-                        break;
-                    case "VIEW_ITEM":
-                        $row->event = "Viewed Item";
-                        $row->object = $itemsH[$r[$j]->event_detail_1]->name;
-                        $row->timestamp = $r[$j]->timestamp;
-                        $row->human = $playerLogs[$i]->player->display_name." viewed ".$row->object." (Item).";
-                        break;
-                    case "VIEW_NODE":
-                        $row->event = "Viewed Node";
-                        $row->object = $nodesH[$r[$j]->event_detail_1]->title;
-                        $row->timestamp = $r[$j]->timestamp;
-                        $row->human = $playerLogs[$i]->player->display_name." viewed ".$row->object." (Node).";
-                        break;
-                    case "VIEW_NPC":
-                        $row->event = "Viewed NPC";
-                        $row->object = $npcsH[$r[$j]->event_detail_1]->name;
-                        $row->timestamp = $r[$j]->timestamp;
-                        $row->human = $playerLogs[$i]->player->display_name." viewed ".$row->object." (Npc).";
-                        break;
-                    case "VIEW_WEBPAGE":
-                        $row->event = "Viewed Web Page";
-                        $row->object = $webpagesH[$r[$j]->event_detail_1]->name;
-                        $row->timestamp = $r[$j]->timestamp;
-                        $row->human = $playerLogs[$i]->player->display_name." viewed ".$row->object." (Web Page).";
-                        break;
-                    case "ENTER_QRCODE":
-                        $row->event = "Entered QR";
-                        $row->code = $r[$j]->event_detail_1;
-                        $row->object = $locationsH[$qrcodesH[$r[$j]->event_detail_1]->link_id]->name;
-                        $row->timestamp = $r[$j]->timestamp;
-                        $row->human = $playerLogs[$i]->player->display_name." scanned ".$row->object.".";
-                        break;
-                    case "COMPLETE_QUEST":
-                        $row->event = "Completed Quest";
-                        $row->object = $questsH[$r[$j]->event_detail_1]->name;
-                        $row->timestamp = $r[$j]->timestamp;
-                        $row->human = $playerLogs[$i]->player->display_name." completed quest '".$row->object."'.";
-                        break;
-                    case "VIEW_MAP":
-                        $row->event = "Viewed Map";
-                        $row->timestamp = $r[$j]->timestamp;
-                        $row->human = $playerLogs[$i]->player->display_name." viewed the map.";
-                        break;
-                    case "VIEW_QUESTS":
-                        $row->event = "Viewed Quests";
-                        $row->timestamp = $r[$j]->timestamp;
-                        $row->human = $playerLogs[$i]->player->display_name." viewed the quests.";
-                        break;
-                    case "VIEW_INVENTORY":
-                        $row->event = "Viewed Inventory";
-                        $row->timestamp = $r[$j]->timestamp;
-                        $row->human = $playerLogs[$i]->player->display_name." viewed the inventory.";
-                        break;
-                    case "MOVE":
-                        $row->event = "Moved";
-                        $row->lat = $r[$j]->event_detail_1;
-                        $row->lon = $r[$j]->event_detail_2;
-                        $row->timestamp = $r[$j]->timestamp;
-                        $row->human = $playerLogs[$i]->player->display_name." moved to (".$row->lat.($reqOutputFormat == "csv" ? " " : ",").$row->lon.")";
-                        break;
-                    case "RECEIVE_WEBHOOK":
-                        $row->event = "Received Hook";
-                        $row->object = $webhooksH[$r[$j]->event_detail_1]->name;
-                        $row->timestamp = $r[$j]->timestamp;
-                        $row->human = $playerLogs[$i]->player->display_name." received hook '".$row->object."'";
-                        break;
-                    default:
-                        $row->event = $r[$j]->event_type;
-                        $row->timestamp = $r[$j]->timestamp;
-                        $row->human = $playerLogs[$i]->player->display_name." ".$row->event;
-                        break;
-                }
-                if($reqVerbose)
-                {
-                    $row->raw = new stdClass();
-                    $row->raw->id             = $r[$j]->id;
-                    $row->raw->player_id      = $r[$j]->player_id;
-                    $row->raw->game_id        = $r[$j]->game_id;
-                    $row->raw->timestamp      = $r[$j]->timestamp;
-                    $row->raw->event_type     = $r[$j]->event_type;
-                    $row->raw->event_detail_1 = $r[$j]->event_detail_1;
-                    $row->raw->event_detail_2 = $r[$j]->event_detail_2;
-                    $row->raw->event_detail_3 = $r[$j]->event_detail_3;
-                    $row->raw->deleted        = $r[$j]->deleted;
-                }
+ 	//used to segment writes so not too much memory is used
+	$done = false;
+	$append = false;
+	$includeHeader = true;
+	$pagesize = 1000;
+	$currecs = 0;
+	while(!$done)
+	{
+		for($i = 0; $i < count($playerLogs); $i++)
+		{
+		    $playerLogs[$i]->log = array();
+		    $r = Module::queryArray("SELECT * FROM player_log WHERE player_id = '{$playerLogs[$i]->player->player_id}' AND game_id = '{$reqGameId}' AND  timestamp BETWEEN '{$reqStartDate}' AND '{$reqEndDate}' AND (deleted = 0 OR deleted = {$reqGetExpired}) LIMIT {$currecs},{$pagesize};");
+		    $currecs += count($r);
+		    if(count($r) == 0) $done = true;
+		    for($j = 0; $j < count($r); $j++)
+		    {
+			$row = new stdClass();
+			switch($r[$j]->event_type)
+			{
+			    case "PICKUP_ITEM":
+				$row->event = "Received Item";
+				$row->object = $itemsH[$r[$j]->event_detail_1]->name;
+				$row->qty = $r[$j]->event_detail_2;
+				$row->timestamp = $r[$j]->timestamp;
+				$row->human = $playerLogs[$i]->player->display_name." received ".$row->qty." ".$row->object." (Item).";
+				break;
+			    case "DROP_ITEM":
+			    case "DESTROY_ITEM":
+				$row->event = "Lost Item";
+				$row->object = $itemsH[$r[$j]->event_detail_1]->name;
+				$row->qty = $r[$j]->event_detail_2;
+				$row->timestamp = $r[$j]->timestamp;
+				$row->human = $playerLogs[$i]->player->display_name." lost ".$row->qty." ".$row->object." (Item).";
+				break;
+			    case "VIEW_ITEM":
+				$row->event = "Viewed Item";
+				$row->object = $itemsH[$r[$j]->event_detail_1]->name;
+				$row->timestamp = $r[$j]->timestamp;
+				$row->human = $playerLogs[$i]->player->display_name." viewed ".$row->object." (Item).";
+				break;
+			    case "VIEW_NODE":
+				$row->event = "Viewed Node";
+				$row->object = $nodesH[$r[$j]->event_detail_1]->title;
+				$row->timestamp = $r[$j]->timestamp;
+				$row->human = $playerLogs[$i]->player->display_name." viewed ".$row->object." (Node).";
+				break;
+			    case "VIEW_NPC":
+				$row->event = "Viewed NPC";
+				$row->object = $npcsH[$r[$j]->event_detail_1]->name;
+				$row->timestamp = $r[$j]->timestamp;
+				$row->human = $playerLogs[$i]->player->display_name." viewed ".$row->object." (Npc).";
+				break;
+			    case "VIEW_WEBPAGE":
+				$row->event = "Viewed Web Page";
+				$row->object = $webpagesH[$r[$j]->event_detail_1]->name;
+				$row->timestamp = $r[$j]->timestamp;
+				$row->human = $playerLogs[$i]->player->display_name." viewed ".$row->object." (Web Page).";
+				break;
+			    case "ENTER_QRCODE":
+				$row->event = "Entered QR";
+				$row->code = $r[$j]->event_detail_1;
+				$row->object = $locationsH[$qrcodesH[$r[$j]->event_detail_1]->link_id]->name;
+				$row->timestamp = $r[$j]->timestamp;
+				$row->human = $playerLogs[$i]->player->display_name." scanned ".$row->object.".";
+				break;
+			    case "COMPLETE_QUEST":
+				$row->event = "Completed Quest";
+				$row->object = $questsH[$r[$j]->event_detail_1]->name;
+				$row->timestamp = $r[$j]->timestamp;
+				$row->human = $playerLogs[$i]->player->display_name." completed quest '".$row->object."'.";
+				break;
+			    case "VIEW_MAP":
+				$row->event = "Viewed Map";
+				$row->timestamp = $r[$j]->timestamp;
+				$row->human = $playerLogs[$i]->player->display_name." viewed the map.";
+				break;
+			    case "VIEW_QUESTS":
+				$row->event = "Viewed Quests";
+				$row->timestamp = $r[$j]->timestamp;
+				$row->human = $playerLogs[$i]->player->display_name." viewed the quests.";
+				break;
+			    case "VIEW_INVENTORY":
+				$row->event = "Viewed Inventory";
+				$row->timestamp = $r[$j]->timestamp;
+				$row->human = $playerLogs[$i]->player->display_name." viewed the inventory.";
+				break;
+			    case "MOVE":
+				$row->event = "Moved";
+				$row->lat = $r[$j]->event_detail_1;
+				$row->lon = $r[$j]->event_detail_2;
+				$row->timestamp = $r[$j]->timestamp;
+				$row->human = $playerLogs[$i]->player->display_name." moved to (".$row->lat.($reqOutputFormat == "csv" ? " " : ",").$row->lon.")";
+				break;
+			    case "RECEIVE_WEBHOOK":
+				$row->event = "Received Hook";
+				$row->object = $webhooksH[$r[$j]->event_detail_1]->name;
+				$row->timestamp = $r[$j]->timestamp;
+				$row->human = $playerLogs[$i]->player->display_name." received hook '".$row->object."'";
+				break;
+			    default:
+				$row->event = $r[$j]->event_type;
+				$row->timestamp = $r[$j]->timestamp;
+				$row->human = $playerLogs[$i]->player->display_name." ".$row->event;
+				break;
+			}
+			if($reqVerbose)
+			{
+			    $row->raw = new stdClass();
+			    $row->raw->id             = $r[$j]->id;
+			    $row->raw->player_id      = $r[$j]->player_id;
+			    $row->raw->game_id        = $r[$j]->game_id;
+			    $row->raw->timestamp      = $r[$j]->timestamp;
+			    $row->raw->event_type     = $r[$j]->event_type;
+			    $row->raw->event_detail_1 = $r[$j]->event_detail_1;
+			    $row->raw->event_detail_2 = $r[$j]->event_detail_2;
+			    $row->raw->event_detail_3 = $r[$j]->event_detail_3;
+			    $row->raw->deleted        = $r[$j]->deleted;
+			}
 
-                $playerLogs[$i]->log[] = $row;
-            }
-        }
-        if($reqOutputFormat == "json")
-            return new returnData(0,$playerLogs);
-        if($reqOutputFormat == "csv")
-        {
-            $csv = "";
+			$playerLogs[$i]->log[] = $row;
+		    }
+		}
+		if($reqOutputFormat == "json")
+		    return new returnData(0,$playerLogs);
+		if($reqOutputFormat == "csv")
+		{
+		    $csv = "";
 
-            if($includeHeader)
-            {
-                $csv .= "group_name,";
-                $csv .= "player_id,";
-                $csv .= "display_name,";
-                $csv .= "timestamp,";
-                $csv .= "human".($reqVerbose ? "," : "\n" );
-                if($reqVerbose)
-                {
-                $csv .= "player_log_id,";
-                $csv .= "player_id,";
-                $csv .= "game_id,";
-                $csv .= "timestamp,";
-                $csv .= "event_type,";
-                $csv .= "event_detail_1,";
-                $csv .= "event_detail_2,";
-                $csv .= "event_detail_3,";
-                $csv .= "deleted\n";
-                }
-            }
+		    if($includeHeader)
+		    {
+			$csv .= "group_name,";
+			$csv .= "player_id,";
+			$csv .= "display_name,";
+			$csv .= "timestamp,";
+			$csv .= "human".($reqVerbose ? "," : "\n" );
+			if($reqVerbose)
+			{
+			$csv .= "player_log_id,";
+			$csv .= "player_id,";
+			$csv .= "game_id,";
+			$csv .= "timestamp,";
+			$csv .= "event_type,";
+			$csv .= "event_detail_1,";
+			$csv .= "event_detail_2,";
+			$csv .= "event_detail_3,";
+			$csv .= "deleted\n";
+			}
+		    }
 
-            for($i = 0; $i < count($playerLogs); $i++)
-            {
-                for($j = 0; $j < count($playerLogs[$i]->log); $j++)
-                {
-                    $csv .= $playerLogs[$i]->player->group_name.",";
-                    $csv .= $playerLogs[$i]->player->player_id.",";
-                    $csv .= $playerLogs[$i]->player->display_name.",";
-                    $csv .= $playerLogs[$i]->log[$j]->timestamp.",";
-                    $csv .= $playerLogs[$i]->log[$j]->human. ( $reqVerbose ? "," : "\n" );
-                    if($reqVerbose)
-                    {
-                    $csv .= $playerLogs[$i]->log[$j]->raw->id.",";
-                    $csv .= $playerLogs[$i]->log[$j]->raw->player_id.",";
-                    $csv .= $playerLogs[$i]->log[$j]->raw->game_id.",";
-                    $csv .= $playerLogs[$i]->log[$j]->raw->timestamp.",";
-                    $csv .= $playerLogs[$i]->log[$j]->raw->event_type.",";
-                    $csv .= $playerLogs[$i]->log[$j]->raw->event_detail_1.",";
-                    $csv .= $playerLogs[$i]->log[$j]->raw->event_detail_2.",";
-                    $csv .= $playerLogs[$i]->log[$j]->raw->event_detail_3.",";
-                    $csv .= $playerLogs[$i]->log[$j]->raw->deleted."\n";
-                    }
-                }
-            }
-        }
+		    for($i = 0; $i < count($playerLogs); $i++)
+		    {
+			for($j = 0; $j < count($playerLogs[$i]->log); $j++)
+			{
+			    $csv .= $playerLogs[$i]->player->group_name.",";
+			    $csv .= $playerLogs[$i]->player->player_id.",";
+			    $csv .= $playerLogs[$i]->player->display_name.",";
+			    $csv .= $playerLogs[$i]->log[$j]->timestamp.",";
+			    $csv .= $playerLogs[$i]->log[$j]->human. ( $reqVerbose ? "," : "\n" );
+			    if($reqVerbose)
+			    {
+			    $csv .= $playerLogs[$i]->log[$j]->raw->id.",";
+			    $csv .= $playerLogs[$i]->log[$j]->raw->player_id.",";
+			    $csv .= $playerLogs[$i]->log[$j]->raw->game_id.",";
+			    $csv .= $playerLogs[$i]->log[$j]->raw->timestamp.",";
+			    $csv .= $playerLogs[$i]->log[$j]->raw->event_type.",";
+			    $csv .= $playerLogs[$i]->log[$j]->raw->event_detail_1.",";
+			    $csv .= $playerLogs[$i]->log[$j]->raw->event_detail_2.",";
+			    $csv .= $playerLogs[$i]->log[$j]->raw->event_detail_3.",";
+			    $csv .= $playerLogs[$i]->log[$j]->raw->deleted."\n";
+			    }
+			}
+		    }
+		}
 
-        file_put_contents(Config::gamedataFSPath."/".$reqGameId."/mostrecentlogrequest.csv",$csv, ($append ? FILE_APPEND : 0));
+        	file_put_contents(Config::gamedataFSPath."/".$reqGameId."/mostrecentlogrequest.csv",$csv, ($append ? FILE_APPEND : 0));
+		$append = true;
+		$includeHeader = false;
+	}
 
         return new returnData(0,Config::gamedataWWWPath."/".$reqGameId."/mostrecentlogrequest.csv");
     }
 
-    public function getPlayerLogsSegmented($glob)
-    {
-        //Grrr amfphp should take care of this...
-	$data = file_get_contents("php://input");
-        $glob = json_decode($data);
-
-        $reqStartDate = $glob->start_date;
-        $reqEndDate   = $glob->end_date;
-
-        if(!preg_match("/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/",$reqStartDate)) $reqStartDate = "2015-01-00 00:00:00";
-        if(!preg_match("/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/",$reqEndDate))   $reqEndDate   = "2015-02-00 00:00:00";
-
-        $start = date_create($reqStartDate);
-        $end   = date_create($reqEndDate);
-
-        $dates = array();
-        $ret = "";
-        while($start < $end)
-        {
-            $ret = PlayerLog::getPlayerLogsGlob($glob);
-            $glob->append = 1;
-            $glob->include_header = 0;
-            $start = strtotime("+1 day",$start);
-        }
-
-        return $ret;
-    }
 }
 ?>
