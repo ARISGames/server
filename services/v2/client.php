096327e0f1afdf5f8c467dbe7d2f63ba5b97b1bb
Phildo <phildo211@gmail.com>
2015-06-17 17:56:39 +0000

    events as loggable instances

--- services/v2/client.php
+++ services/v2/client.php
@@ -644,6 +644,15 @@ class client extends dbconnection
         return new return_package(0);
     }
 
+    public static function logPlayerRanEventPackage($pack)
+    {
+        $pack->auth->permission = "read_write";
+        if(!users::authenticateUser($pack->auth)) return new return_package(6, NULL, "Failed Authentication");
+
+        dbconnection::queryInsert("INSERT INTO user_log (user_id, game_id, event_type, content_id, created) VALUES ('{$pack->auth->user_id}', '{$pack->game_id}', 'RUN_EVENT_PACKAGE_ID', '{$pack->event_package_id}', CURRENT_TIMESTAMP);");
+        return new return_package(0);
+    }
+
     public static function logPlayerCreatedNote($pack)
     {
         $pack->auth->permission = "read_write";
