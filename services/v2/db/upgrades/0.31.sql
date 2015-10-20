2ce1a3b800de2a8a09415eb8e718f5e008243bb4
Michael Tolly <miketolly@gmail.com>
2015-10-20 13:01:03 -0500

    put db upgrade query on 1 line

--- services/v2/db/upgrades/0.31.sql
+++ services/v2/db/upgrades/0.31.sql
@@ -1,11 +1,3 @@
-CREATE TABLE `colors` (
-  `colors_id` int(32) unsigned NOT NULL AUTO_INCREMENT,
-  `tag_1` varchar(255) DEFAULT '',
-  `tag_2` varchar(255) DEFAULT '',
-  `tag_3` varchar(255) DEFAULT '',
-  `tag_4` varchar(255) DEFAULT '',
-  `tag_5` varchar(255) DEFAULT '',
-  PRIMARY KEY (`colors_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `colors` ( `colors_id` int(32) unsigned NOT NULL AUTO_INCREMENT, `tag_1` varchar(255) DEFAULT '', `tag_2` varchar(255) DEFAULT '', `tag_3` varchar(255) DEFAULT '', `tag_4` varchar(255) DEFAULT '', `tag_5` varchar(255) DEFAULT '', PRIMARY KEY (`colors_id`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `games` ADD `colors_id` INT(32)  UNSIGNED  NULL  DEFAULT NULL  AFTER `last_active`;
