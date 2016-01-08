4088673f0a13e8ba797cbf9ffc10b7b70bf1e73f
Michael Tolly <miketolly@gmail.com>
2016-01-08 15:58:49 -0600

    Most of the backpack API changes
    The inventory items don't have tags yet

--- services/v2/backpack.php
+++ services/v2/backpack.php
@@ -9,16 +9,29 @@ class backpack extends dbconnection
     public static function getGroupUsers($pack)
     {
         /* Call 1:
-        - They give ARIS a group_id
-        - group_id gives player_id, player_pic, and in_game_name
+        - They give ARIS a group_name and game_ids
+        - Returns an array of (player_id, player_pic, player_thumb, in_game_name)
         */
-        $group_id = intval($pack->group_id);
+        $group_name = addslashes($pack->group_name);
+        $game_ids = $pack->game_ids;
+        if (is_array($game_ids)) {
+            $game_ids = array_map('intval', $game_ids);
+        } else {
+            $game_ids = array(intval($game_ids));
+        }
+        if (empty($game_ids)) {
+            return new return_package(0, array());
+        }
+        $game_ids = implode(',', $game_ids);
+
         $q = "SELECT users.user_id, users.display_name, users.media_id
             , media.file_name, media.file_folder
-            FROM user_game_groups
-            JOIN users ON users.user_id = user_game_groups.user_id
+            FROM users
             LEFT JOIN media ON users.media_id = media.media_id
-            WHERE user_game_groups.group_id = {$group_id}
+            INNER JOIN user_log ON users.user_id = user_log.user_id
+            WHERE users.group_name = \"{$group_name}\"
+            AND user_log.game_id IN ({$game_ids})
+            GROUP BY users.user_id
             ";
         $users = dbconnection::queryArray($q);
         if ($users === false) $users = array();
@@ -29,9 +42,11 @@ class backpack extends dbconnection
             $new_user->player_id = intval($user->user_id);
             if ($user->media_id) {
                 $media = media::mediaObjectFromSQL($user);
-                $new_user->player_pic = $media->url; // what about thumb_url ?
+                $new_user->player_pic = $media->url;
+                $new_user->player_thumb = $media->thumb_url;
             } else {
                 $new_user->player_pic = null;
+                $new_user->player_thumb = null;
             }
             $new_user->in_game_name = $user->display_name;
             $new_users[] = $new_user;
@@ -43,21 +58,18 @@ class backpack extends dbconnection
     public static function getUserBackpack($pack)
     {
         /* Call 2:
-        - they give ARIS a player_id, and auth for an owner of 1 or more games
-        - they get player_id, player_pic, and in_game_name,
+        - they give ARIS a player_id, and a game_ids array
+        - they get player_id, player_pic, player_thumb, and in_game_name,
           then the following indexed by game_id:
-          (inventory, active/completed quests, notes)
+          (inventory, active/completed quests)
         */
-        $pack->auth->permission = "read_write";
-        if(!users::authenticateUser($pack->auth)) return new return_package(6, NULL, "Failed Authentication");
-
-        $user_id = intval($pack->user_id);
+        $player_id = intval($pack->player_id);
 
         $q = "SELECT users.user_id, users.display_name, users.media_id
             , media.file_name, media.file_folder
             FROM users
             LEFT JOIN media ON users.media_id = media.media_id
-            WHERE users.user_id = {$user_id}
+            WHERE users.user_id = {$player_id}
             ";
         $sql_user = dbconnection::queryObject($q);
         if ($sql_user === false) return new return_package(6, NULL, "User not found");
@@ -65,28 +77,31 @@ class backpack extends dbconnection
         $backpack->player_id = intval($sql_user->user_id);
         if ($sql_user->media_id) {
             $media = media::mediaObjectFromSQL($sql_user);
-            $backpack->player_pic = $media->url; // what about thumb_url ?
+            $backpack->player_pic = $media->url;
+            $backpack->player_thumb = $media->thumb_url;
         } else {
             $backpack->player_pic = null;
+            $backpack->player_thumb = null;
         }
         $backpack->in_game_name = $sql_user->display_name;
 
-        $owner_id = intval($pack->auth->user_id);
-        $q = "SELECT game_id FROM user_games WHERE user_id = {$owner_id}";
-        $games = dbconnection::queryArray($q);
-        if ($games === false) $games = array();
+        $game_ids = $pack->game_ids;
+        if (is_array($game_ids)) {
+            $game_ids = array_map('intval', $game_ids);
+        } else {
+            $game_ids = array(intval($game_ids));
+        }
 
         $backpack->games = array();
-        foreach ($games as $game) {
-            $game_id = intval($game->game_id);
-
-            $q = "SELECT object_id, qty
+        foreach ($game_ids as $game_id) {
+            $q = "SELECT instances.object_id, instances.qty, items.name, items.type
                 FROM instances
-                WHERE game_id = {$game_id}
-                AND object_type = 'ITEM'
-                AND owner_type = 'USER'
-                AND owner_id = {$user_id}
-                AND qty > 0
+                INNER JOIN items ON items.item_id = instances.object_id
+                WHERE instances.game_id = {$game_id}
+                AND instances.object_type = 'ITEM'
+                AND instances.owner_type = 'USER'
+                AND instances.owner_id = {$user_id}
+                AND instances.qty > 0
                 ";
             $inventory = dbconnection::queryArray($q);
             if ($inventory === false) $inventory = array();
@@ -109,28 +124,9 @@ class backpack extends dbconnection
                 $quests[] = intval($quest->content_id);
             }
 
-            $q = "SELECT notes.*, media.file_name, media.file_folder
-                FROM notes
-                LEFT JOIN media ON notes.media_id = media.media_id
-                WHERE notes.user_id = {$user_id}
-                AND notes.game_id = {$game_id}
-                ";
-            $notes = dbconnection::queryArray($q);
-            if ($notes === false) $notes = array();
-            foreach ($notes as $note) {
-                if ($note->media_id) {
-                    $note->media = media::mediaObjectFromSQL($note);
-                }
-                unset($note->file_name);
-                unset($note->file_folder);
-                unset($note->media_id);
-                $note->note_id = intval($note->note_id);
-            }
-
             $result = new stdClass();
             $result->inventory = $inventory;
             $result->quests = $quests;
-            $result->notes = $notes;
             $backpack->games[$game_id] = $result;
         }
 
