f3bdcc2a6cb894ec692ede17bbe905a55f7c6dca
Michael Tolly <miketolly@gmail.com>
2016-02-16 12:48:46 -0600

    Fix db upgrade file...

--- services/v2/db/upgrades/0.34.sql
+++ services/v2/db/upgrades/0.34.sql
@@ -1,4 +1,1 @@
-ALTER TABLE `colors`
-ADD `tag_6` varchar(255) DEFAULT '' AFTER `tag_5`,
-ADD `tag_7` varchar(255) DEFAULT '' AFTER `tag_6`,
-ADD `tag_8` varchar(255) DEFAULT '' AFTER `tag_7`;
+ALTER TABLE `colors` ADD `tag_6` varchar(255) DEFAULT '' AFTER `tag_5`, ADD `tag_7` varchar(255) DEFAULT '' AFTER `tag_6`, ADD `tag_8` varchar(255) DEFAULT '' AFTER `tag_7`;
