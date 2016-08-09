e8980b7c92aa5e3d9e51bb5527d64e5f8c7148fc
Phildo <Phildo211@gmail.com>
2016-08-09 16:28:26 -0500

    fixed == in queries

--- services/v2/test.php
+++ services/v2/test.php
@@ -6,7 +6,6 @@ class test extends dbconnection
 {
     public static function doTest($pack)
     {
-      echo("f".$pack->from."\nt".$pack->to."\n");
       //get quick estimate of # games w/o NOTEBOOK tab (last check = 630/8,000)
       //SELECT games.game_id, ntabs.type FROM games LEFT JOIN (SELECT tabs.game_id, tabs.type FROM tabs WHERE type = 'NOTEBOOK') as ntabs ON games.game_id = ntabs.game_id WHERE ntabs.type IS NULL group by games.game_id;
 
@@ -25,12 +24,12 @@ class test extends dbconnection
         $game = $games[$i];
 
         //figure out if ought be remote
-        $should_be_empty = dbconnection::queryArray("SELECT * FROM tabs WHERE game_id = '".$game->game_id."' && type == 'NOTEBOOK';");
-        if(count($should_be_empty)) { $ought_be_remote = true; $n_notebookd++; }
+        $should_be_empty = dbconnection::queryArray("SELECT * FROM tabs WHERE game_id = '".$game->game_id."' && type = 'NOTEBOOK';");
+        if(count($should_be_empty)>0) { $ought_be_remote = true; $n_notebookd++; }
         $should_be_empty = dbconnection::queryArray("SELECT * FROM factories WHERE game_id = '".$game->game_id."';");
-        if(count($should_be_empty)) { $ought_be_remote = true; $n_factoried++; }
-        $should_be_empty = dbconnection::queryArray("SELECT * FROM instances WHERE game_id = '".$game->game_id."' && (owner_type == 'GAME' || owner_type == 'GROUP') && qty > 0;");
-        if(count($should_be_empty)) { $ought_be_remote = true; $n_instanced++; }
+        if(count($should_be_empty)>0) { $ought_be_remote = true; $n_factoried++; }
+        $should_be_empty = dbconnection::queryArray("SELECT * FROM instances WHERE game_id = '".$game->game_id."' && (owner_type = 'GAME' || owner_type = 'GROUP') && qty > 0;");
+        if(count($should_be_empty)>0) { $ought_be_remote = true; $n_instanced++; }
 
         //if($ought_be_remote) dbconnection::query("UPDATE games SET network_level = 'REMOTE'       WHERE game_id = '".$game->game_id."';");
         //else                 dbconnection::query("UPDATE games SET network_level = 'REMOTE_WRITE' WHERE game_id = '".$game->game_id."';");
