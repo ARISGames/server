36986916a8077ee62548133d48eb0ac29e5d67fe
Michael Tolly <miketolly@gmail.com>
2017-03-23 13:15:28 -0500

    Speed up backpack.getUserBackpack query

--- services/v2/backpack.php
+++ services/v2/backpack.php
@@ -137,6 +137,7 @@ class backpack extends dbconnection
                 AND instances.owner_type = 'USER'
                 AND instances.owner_id = {$player_id}
                 AND instances.qty > 0
+                AND object_tags.game_id = {$game_id}
                 GROUP BY instances.object_id
                 ";
             $inventory = dbconnection::queryArray($q);
