6e8f49d569062101391d16af48e28edfab99d8d7
Michael Tolly <miketolly@gmail.com>
2018-03-07 11:39:58 -0600

    Add form fields

--- services/v2/fields.php
+++ services/v2/fields.php
@@ -35,9 +35,39 @@ class fields extends dbconnection
         return new return_package(0, $data);
     }
 
+    public static function createField($pack)
+    {
+        $game_id = intval($pack->game_id);
+        $pack->game_id = $game_id;
+        $pack->auth->game_id = $game_id;
+        $pack->auth->permission = "read_write";
+        if(!editors::authenticateGameEditor($pack->auth)) return new return_package(6, NULL, "Failed Authentication");
+
+        $columns = array('game_id', 'field_type');
+        $values = array($pack->game_id, $pack->field_type);
+        foreach (array('label', 'required') as $column) {
+            if (isset($pack->$column)) {
+                $columns[] = $column;
+                $values[] = $pack->$column;
+            }
+        }
+        $columns = implode(",", $columns);
+        $quoted = array();
+        foreach ($values as $value) {
+            $quoted[] = '"'.addslashes($value).'"';
+        }
+        $quoted = implode(",", $quoted);
+
+        $field_id = dbconnection::queryInsert("INSERT INTO fields ($columns) VALUES ($quoted)");
+
+        games::bumpGameVersion($pack);
+        return new return_package(0); // could return field but siftr editor will just update call getFieldsForGame
+    }
+
     public static function deleteField($pack)
     {
         $game_id = intval($pack->game_id);
+        $pack->game_id = $game_id;
         $field_id = intval($pack->field_id);
 
         $pack->auth->game_id = $game_id;
@@ -54,6 +84,7 @@ class fields extends dbconnection
             return new return_package(1, NULL, "Field not found");
         }
 
+        games::bumpGameVersion($pack);
         return new return_package(0);
     }
 }
